a20f31c0c005d02cee835fea45bec5821d37b1db
Jordan Dysart <jordandysart@live.ca>
2019-04-18 16:59:54 -0500

    starting over again

--- theme-grabber.js
+++ theme-grabber.js
@@ -1,39 +1,5 @@
 // Higgy's ultimate theme id copy tool
 // April 6 2019
 
-
-function themeDataGet(){
-  // Let's make a sandwich.
-  var balogna      = document.getElementsByClassName("bold__extension_hub-copy-hidden-input");
-  var str = "";
-  for (let item of balogna) {
-    stringCheese += item.value;
-  }
-  return stringCheese;
-}
-
-
-function copyBoldShopID() {
-  copy(() => );
-}
-
-copyDatafrom(themeDataGet);
-
-javascript:(function(){var balogna = document.getElementsByClassName("bold__extension_hub-copy-hidden-input"); var stringCheese = ""; for (let item of balogna) { stringCheese += item.value; }; return stringCheese;})
-
-
-const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-};
-
-// function themeDataGet(){
-//     var name = document.getElementsByClassName("bold__extension_hub-copy-hidden-input")[0].value;
-//     var id = document.getElementsByClassName("bold__extension_hub-copy-hidden-input")[1].value;
-//     themedata = name + " " + id;
-//     return themedata;
-// }
+// Edit: All code has been erased. I'm starting from the beginning.
+// April 18th 2019
