369e39adbb11098d1113b2337fccb72493a76462
Jordan Dysart <jordandysart@live.ca>
2019-04-18 18:20:52 -0500

    after a few iterations and some assistance from http://subsimple.com/bookmarklets/jsbuilder.htm i got something working.

--- theme-grabber.js
+++ theme-grabber.js
@@ -3,3 +3,28 @@
 
 // Edit: All code has been erased. I'm starting from the beginning.
 // April 18th 2019
+
+// var themeName = Shopify.theme.slice(0,1);
+// javascript:
+//   var themeID = Shopify.theme.name + " (" + Shopify.theme.id + ")";
+//   const el = document.createElement('textarea');
+//   el.value = themeID;
+//   document.body.appendChild(el);
+//   el.select();
+//   document.execCommand('copy');
+//   document.body.removeChild(el);
+
+
+// javascript:(function(){
+//   var themeID = Shopify.theme.name + " (" + Shopify.theme.id + ")";
+//   const el = document.createElement('textarea');
+//   el.value = themeID;
+//   document.body.appendChild(el);
+//   el.select();
+//   document.execCommand('copy');
+//   document.body.removeChild(el);
+// })()
+
+// javascript:var themeID%3DShopify.theme.name%2B" ("%2BShopify.theme.id%2B")"%3Bconstel%3Ddocument.createElement(%27textarea%27)%3Bel.value%3DthemeID%3Bdocument.body.appendChild(el)%3Bel.select()%3Bdocument.execCommand(%27copy%27)%3Bdocument.body.removeChild(el)%3B
+
+javascript:(function()%7Bvar themeID%3DShopify.theme.name%2B" ("%2BShopify.theme.id%2B")"%3Bvar el%3Ddocument.createElement(%27textarea%27)%3Bel.value%3DthemeID%3Bdocument.body.appendChild(el)%3Bel.select()%3Bdocument.execCommand(%27copy%27)%3Bdocument.body.removeChild(el)%3B%7D)()
